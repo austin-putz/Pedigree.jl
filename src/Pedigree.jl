85bbe5e24e87d40b077c98d58de0bc205c5c1986
austin-putz <putz.austin@gmail.com>
2025-05-15 14:29:47 -0500

    updated a few files for merge

--- src/Pedigree.jl
+++ src/Pedigree.jl
@@ -4,21 +4,19 @@
 
 module Pedigree
 
-# load packages
+# load necessary packages
 using DataFrames
 
-# include functions
+# include functions from other files in the package
 include("stack_ancestors.jl")
 include("sort_ped.jl")
 include("renum_ped.jl")
 include("makeA.jl")
 
-# export function
+# export functions for use outside the module
 export stack_ancestors
 export sort_ped
 export renum_ped
 export makeA
 
-end
-
-
+end # end of Pedigree module
